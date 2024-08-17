bfd2a1b07a0160884b6943c90ecce9e73ace02ad
Leander-Antony <155832245+Leander-Antony@users.noreply.github.com>
2024-08-18 00:39:29 +0530

    code for creating playlist using user prompt

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.cache_handler import FlaskSessionCacheHandler
 
-from flask import Flask, session, redirect, url_for, request
+from flask import Flask, session, redirect, url_for, request, jsonify
 
 load_dotenv()
 client_id = os.getenv('CLIENT_ID')
@@ -48,11 +48,46 @@ def get_playlists():
         return redirect(auth_url)
     
     playlists = sp.current_user_playlists()
-    print(playlists)
-    playlists_info = [(pl['name'], pl['external_urls']['spotify'], pl) for pl in playlists['items']]
-    playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
+    playlists_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlists['items']]
+    playlists_html = '<br>'.join([f'{name}: <a href="{url}" target="_blank">Open Playlist</a>' for name, url in playlists_info])
+    
+    # HTML for creating a new playlist
+    create_playlist_form = '''
+    <h2>Create a New Playlist</h2>
+    <form action="/create_playlist" method="post">
+        <label for="name">Playlist Name:</label>
+        <input type="text" id="name" name="name" required>
+        <br>
+        <label for="description">Description:</label>
+        <input type="text" id="description" name="description">
+        <br>
+        <label for="public">Public:</label>
+        <input type="checkbox" id="public" name="public" checked>
+        <br>
+        <input type="submit" value="Create Playlist">
+    </form>
+    '''
+    
+    return playlists_html + create_playlist_form
 
-    return playlists_html
+
+@app.route('/create_playlist', methods=['POST'])
+def create_playlist():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    
+    user_id = sp.current_user()['id']
+    playlist_name = request.form.get('name')
+    public = request.form.get('public') == 'on'  
+    description = request.form.get('description', '')
+
+    if not playlist_name:
+        return jsonify({"error": "Playlist name is required"}), 400
+
+    playlist = sp.user_playlist_create(user_id, playlist_name, public=public, description=description)
+    return jsonify(playlist), 201
+
 
 @app.route('/logout')
 def logout():
