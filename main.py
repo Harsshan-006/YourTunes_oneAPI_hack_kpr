8ac415454d28020763c00557b501227ea9fc1b42
Leander-Antony <155832245+Leander-Antony@users.noreply.github.com>
2024-08-17 23:28:54 +0530

    code to get playlists from users

--- main.py
+++ main.py
@@ -5,19 +5,59 @@ from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.cache_handler import FlaskSessionCacheHandler
 
-from flask import Flask, session
+from flask import Flask, session, redirect, url_for, request
+
+load_dotenv()
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.unrandom(64)
+app.config['SECRET_KEY'] = os.urandom(64)
 
-redirect_uri = 'https://localhost:5000/callback'
-scope = "user-read-playback-state, user-modify-playback-state, playlist-read-private, playlist-modify-private, playlist-modify-public, user-top-read"
+redirect_uri = 'http://localhost:5000/callback'
+scope = "playlist-read-private, user-modify-playback-state, user-read-playback-state, playlist-modify-private, playlist-modify-public, user-top-read"
 
 cache_handler = FlaskSessionCacheHandler(session)
 sp_oauth = SpotifyOAuth(
-
-
+    client_id=client_id,
+    client_secret=client_secret,
+    redirect_uri=redirect_uri,
+    scope=scope,
+    cache_handler=cache_handler,
+    show_dialog=True
 )
+
+sp = Spotify(auth_manager=sp_oauth)
+
+@app.route('/')
+def home():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    return redirect(url_for("get_playlists"))
+
+@app.route('/callback')
+def callback():
+    sp_oauth.get_access_token(request.args['code'])
+    return redirect(url_for('get_playlists'))
+
+@app.route('/get_playlists')
+def get_playlists():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    
+    playlists = sp.current_user_playlists()
+    print(playlists)
+    playlists_info = [(pl['name'], pl['external_urls']['spotify'], pl) for pl in playlists['items']]
+    playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
+
+    return playlists_html
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
